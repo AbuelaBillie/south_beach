4c66d798b9aeee755e5fdd236a6c9f9151fed348
ncguede@gmail.com <ncguede@gmail.com>
2022-10-19 10:11:33 -0300

    probando ruta imagenes github

--- scripts/roomSlider.js
+++ scripts/roomSlider.js
@@ -61,51 +61,51 @@ window.addEventListener("load", function () {
 
     //LISTENERS BOTONES SLIDER
     left_btn_0.addEventListener("click", function () {
-        imagen0.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes0,0)}")`
+        imagen0.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes0,0)}")`
     })
     right_btn_0.addEventListener("click", function () {
-        imagen0.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes0,0)}")`
+        imagen0.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes0,0)}")`
     })
     left_btn_1.addEventListener("click", function () {
-        imagen1.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes1,1)}")`
+        imagen1.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes1,1)}")`
     })
     right_btn_1.addEventListener("click", function () {
-        imagen1.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes1,1)}")`
+        imagen1.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes1,1)}")`
     })
     left_btn_2.addEventListener("click", function () {
-        imagen2.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes2,2)}")`
+        imagen2.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes2,2)}")`
     })
     right_btn_2.addEventListener("click", function () {
-        imagen2.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes2,2)}")`
+        imagen2.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes2,2)}")`
     })
     left_btn_3.addEventListener("click", function () {
-        imagen3.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes3,3)}")`
+        imagen3.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes3,3)}")`
     })
     right_btn_3.addEventListener("click", function () {
-        imagen3.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes3,3)}")`
+        imagen3.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes3,3)}")`
     })
     left_btn_4.addEventListener("click", function () {
-        imagen4.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes4,4)}")`
+        imagen4.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes4,4)}")`
     })
     right_btn_4.addEventListener("click", function () {
-        imagen4.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes4,4)}")`
+        imagen4.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes4,4)}")`
     })
     left_btn_5.addEventListener("click", function () {
-        imagen5.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes5,5)}")`
+        imagen5.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes5,5)}")`
     })
     right_btn_5.addEventListener("click", function () {
-        imagen5.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes5,5)}")`
+        imagen5.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes5,5)}")`
     })
     left_btn_6.addEventListener("click", function () {
-        imagen6.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes6,6)}")`
+        imagen6.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes6,6)}")`
     })
     right_btn_6.addEventListener("click", function () {
-        imagen6.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes6,6)}")`
+        imagen6.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes6,6)}")`
     })
     left_btn_7.addEventListener("click", function () {
-        imagen7.style.backgroundImage=`url("./resources/${sliderImagenIzquierda(arrayImagenes7,7)}")`
+        imagen7.style.backgroundImage=`url("./south_beach/resources/${sliderImagenIzquierda(arrayImagenes7,7)}")`
     })
     right_btn_7.addEventListener("click", function () {
-        imagen7.style.backgroundImage=`url("./resources/${sliderImagenDerecha(arrayImagenes7,7)}")`
+        imagen7.style.backgroundImage=`url("./south_beach/resources/${sliderImagenDerecha(arrayImagenes7,7)}")`
     })
 })
