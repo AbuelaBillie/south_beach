a34029514fad1bb9053e49a3aa971d55c135784a
ncguede@gmail.com <ncguede@gmail.com>
2022-10-21 17:00:05 -0300

    continuar en remoto

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -2,10 +2,20 @@ window.addEventListener("load", function () {
     // TRAEMOS ELEMENTOS HTML
     const btnMenu = document.querySelector("#btnMenu");
     const menu = document.querySelector(".navMenu");
+    const main = document.querySelector(".main");
+    const footer = document.querySelector(".footer");
   
     // LISTENER BOTON MENU 
     btnMenu.addEventListener("click", function () {
       menu.classList.toggle("mostrar");
+    });
+  
+    main.addEventListener("click", function () {
+      menu.classList.remove("mostrar");
+    });
+
+    footer.addEventListener("click", function () {
+      menu.classList.remove("mostrar");
     });
   
   });
